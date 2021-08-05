e80c721f03761c0007a5c7fc86989fa1bc618361
JeyFarias <jessica.fn06@aluno.ifsc.edu.br>
2021-08-05 19:17:56 -0300

    Servidor atualizado

--- servidor.js
+++ servidor.js
@@ -2,7 +2,7 @@ var express = require("express")
 var rotas = require("./routes/produtoroutes")
 
 var servidor = express()
-const PORTA = 8081
+const PORTA = 8082
 
 servidor.use(express.urlencoded({extended:true}))
 servidor.use(rotas)
